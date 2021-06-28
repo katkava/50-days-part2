c5d225ec6dea477245cbb742720a95547d9818b7
KatKava <katryn.kav97@gmail.com>
2021-06-28 23:10:09 +0100

    hidden widget day 4

---
+++ hidden-widget/script.js
@@ -0,0 +1,13 @@
+// select btn (click) => function, toggle active class, focus method on input 
+const button = document.querySelector('.btn')
+const search = document.querySelector('.search')
+const input = document.querySelector('.input')
+
+
+
+button.addEventListener('click', () => {
+    search.classList.toggle('active');
+    input.focus();
+})
+ 
+
